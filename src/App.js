bf239b389a2784ceca871f6fff1fb2e22aedd034
andresg19 <andygermain17@gmail.com>
2023-04-25 12:49:34 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="grid">
+      <header>
+      <h1 className=''>meli clone</h1>
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
